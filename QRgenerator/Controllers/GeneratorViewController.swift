eef1e0e374601f46c4c9f9ea0358e4ca31bdc35e
Artjoms Vorona <artjomsvorona@gmail.com>
2019-12-19 23:32:38 +0200

    Core data save/load added for GeneratorViewController.

--- QRgenerator/Controllers/GeneratorViewController.swift
+++ QRgenerator/Controllers/GeneratorViewController.swift
@@ -6,28 +6,39 @@
 //  Copyright © 2019 Artjoms Vorona. All rights reserved.
 //
 
-
+import CoreData
 import UIKit
 
 class GeneratorViewController: UIViewController {
     
+    var items = [Items]()
     var qrImage: UIImage!
+    
+    var context: NSManagedObjectContext?
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var qrTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        context = appDelegate.persistentContainer.viewContext
+        
+        loadData()
     }
     
     @IBAction func saveButtonTouched(_ sender: UIButton) {
         if qrImage != nil {
-            print("There is image")
+            let newItem = Items(context: self.context!)
+            newItem.qrImage = Data(qrImage.pngData()!)
+            newItem.text = qrTextField.text
         } else {
             print("No image to save")
         }
     }
-    @IBAction func generateButtpnTouched(_ sender: UIButton) {
+    
+    @IBAction func generateButtonTouched(_ sender: UIButton) {
         generateQRCode()
     }
     
@@ -50,6 +61,26 @@ class GeneratorViewController: UIViewController {
         let imageResult = UIImage(cgImage: cgImage)
         qrImage = imageResult
         imageView.image = imageResult
+    }
+    
+    //MARK: - Core Data functions
+    
+    func saveData() {
+        do {
+            try context?.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func loadData() {
+        let request: NSFetchRequest<Items> = Items.fetchRequest()
+        
+        do {
+            items = try (context?.fetch(request))!
+        } catch  {
+            print(error.localizedDescription)
+        }
     }
 
 }
